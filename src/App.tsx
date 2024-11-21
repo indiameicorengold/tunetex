01880d7606475a22732a02086646fa18b06bee0b
benhduong <benhduong@gmail.com>
2024-11-20 17:32:57 -0800

    feat: working form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form: HTMLFormElement = e.currentTarget;
+    const formData: FormData = new FormData(form);
+
+    // Or you can work with it as a plain object:
+    const formJson: Record<string, string> = Object.fromEntries(
+      formData.entries()
+    ) as Record<string, string>;
+
+    console.log(formJson);
+  }
+
   return (
     <>
       <h1>Tunetex</h1>
-      <textarea placeholder="paste text in here" rows={20} cols={50}></textarea>
-      <div className="card">
-        <button>generate!</button>
-      </div>
+      <form method="get" onSubmit={handleSubmit}>
+        <textarea
+          name="textarea"
+          placeholder="paste text in here"
+          rows={20}
+          cols={50}
+        />
+        <hr />
+        <button type="submit">generate!</button>
+      </form>
     </>
   );
 }
